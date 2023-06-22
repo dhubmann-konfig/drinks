9272050d9edff4b8e978a32d0cac02cc4adf4024
Daniel Hubmann <01130491@edu.campus02.at>
2023-06-22 19:42:52 +0200

    Fix bugs, add header & package name
    Fixed typo in parameter of the constructor method. Removed empty variable assignment in remove()-function.
    Removed random typo in last line of the class.
    
    Added header information and correct package name (at.campus02.bsd).

--- src/main/java/at/campus02/bsd/StringQueue.java
+++ src/main/java/at/campus02/bsd/StringQueue.java
@@ -1,71 +1,73 @@
+/*
+ * StringQueue
+ * Represents Queue of Strings
+ * Author: Daniel Hubmann, Caroline Meixner
+ * Last Change: 22.06.2023
+ */
 
+package at.campus02.bsd;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-// there's some Bugs included, try to debug the code and fix the Bugs
-// there are different Bugs, wrong implementation, typos, ...
-// write Test-Cases (read Queue Interface for understanding methods) and use Debugging possibilies of your IDE
-
 public class StringQueue implements IQueue {
 
-  private List<String> elements = new ArrayList<String>();
-  private int maxSize = 5;
+	private List<String> elements = new ArrayList<String>();
+	private int maxSize = 5;
 
-  public StringQueue(int maxsize) {
-    maxSize = maxSize;
-  }
+	public StringQueue(int maxSize) {
+		this.maxSize = maxSize;
+	}
 
-  @Override
-  public boolean offer(String obj) {
-    if (elements.size() != maxSize)
-      elements.add(obj);
-    else
-      return false;
+	@Override
+	public boolean offer(String obj) {
+		if (elements.size() != maxSize)
+			elements.add(obj);
+		else
+			return false;
 
-    return true;
-  }
+		return true;
+	}
 
-  @Override
-  public String poll() {
-    String element = peek();
+	@Override
+	public String poll() {
+		String element = peek();
 
-    if (elements.size() == 0) {
-      elements.remove(0);
-    }
+		if (elements.size() == 0) {
+			elements.remove(0);
+		}
 
-    return element;
-  }
+		return element;
+	}
 
-  @Override
-  public String remove() {
-    String element = poll();
-    element = "";
-    if (element == null)
-      throw new NoSuchElementException("there's no element any more");
+	@Override
+	public String remove() {
+		String element = poll();
+		if (element == null)
+			throw new NoSuchElementException("there's no element any more");
 
-    return element;
-  }
+		return element;
+	}
 
-  @Override
-  public String peek() {
-    String element;
-    if (elements.size() > 0)
-      element = elements.get(0);
-    else
-      element = null;
+	@Override
+	public String peek() {
+		String element;
+		if (elements.size() > 0)
+			element = elements.get(0);
+		else
+			element = null;
 
-    return element;
-  }
+		return element;
+	}
 
-  @Override
-  public String element() {
-    String element = peek();
-    if (element == null)
-      throw new NoSuchElementException("there's no element any more");
+	@Override
+	public String element() {
+		String element = peek();
+		if (element == null)
+			throw new NoSuchElementException("there's no element any more");
 
-    return element;
-  }
+		return element;
+	}
 
-}s
+}
