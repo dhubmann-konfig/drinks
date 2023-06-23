2d4af9449f5d89277b7a452349790e811ce97180
Daniel Hubmann <01130491@edu.campus02.at>
2023-06-23 15:38:41 +0200

    Add javadoc to StringQueue class

--- src/main/java/at/campus02/bsd/StringQueue.java
+++ src/main/java/at/campus02/bsd/StringQueue.java
@@ -11,15 +11,44 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+/**
+ * A StringQueue object represents a queue containing String objects.
+ * 
+ * It implements the IQueue interface and therefore contains methods to add,
+ * remove and return the first element (in the front) of the queue.
+ * 
+ * @author Daniel Hubmann
+ *
+ */
 public class StringQueue implements IQueue {
 
+	/**
+	 * List of String objects to store the elements of the queue
+	 */
 	private List<String> elements = new ArrayList<String>();
+
+	/**
+	 * Maximum size of the queue
+	 */
 	private int maxSize = 5;
 
+	/**
+	 * Constructs a new StringQueue object with the specified size
+	 * 
+	 * @author Daniel Hubmann
+	 * @param maxSize The maximum size of the queue
+	 */
 	public StringQueue(int maxSize) {
 		this.maxSize = maxSize;
 	}
 
+	/**
+	 * Adds the specified String to the end of the queue, if the queue is not full.
+	 * 
+	 * @author Daniel Hubmann
+	 * @param obj The String object to be added.
+	 * @return true if object was added successfully, false otherwise
+	 */
 	@Override
 	public boolean offer(String obj) {
 		if (elements.size() != maxSize)
@@ -30,6 +59,12 @@ public class StringQueue implements IQueue {
 		return true;
 	}
 
+	/**
+	 * Returns and removes first element in the front of the queue.
+	 * 
+	 * @author Daniel Hubmann
+	 * @return The first element in the queue or null if the queue is empty
+	 */
 	@Override
 	public String poll() {
 		String element = peek();
@@ -41,6 +76,13 @@ public class StringQueue implements IQueue {
 		return element;
 	}
 
+	/**
+	 * Returns and removes first element in the front of the queue.
+	 * 
+	 * @author Daniel Hubmann
+	 * @return The first element in the queue
+	 * @throws NoSuchElementException if the queue is empty
+	 */
 	@Override
 	public String remove() {
 		String element = poll();
@@ -50,6 +92,13 @@ public class StringQueue implements IQueue {
 		return element;
 	}
 
+	/**
+	 * Returns the first element in the front of the queue if it is not empty, or
+	 * null if it is empty.
+	 * 
+	 * @author Daniel Hubmann
+	 * @return The first element in the queue or null if the queue is empty
+	 */
 	@Override
 	public String peek() {
 		String element;
@@ -61,6 +110,13 @@ public class StringQueue implements IQueue {
 		return element;
 	}
 
+	/**
+	 * Returns the first element in the front of the queue.
+	 * 
+	 * @author Daniel Hubmann
+	 * @return The first element in the queue
+	 * @throws NoSuchElementExcpetion if the queue is empty
+	 */
 	@Override
 	public String element() {
 		String element = peek();
