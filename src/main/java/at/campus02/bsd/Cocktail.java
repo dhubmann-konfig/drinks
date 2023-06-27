3b89ee89e153508a1632467382790a0db150ddb7
Caroline Meixner <01610205@edu.campus02.at>
2023-06-27 15:19:51 +0200

    Create Cocktail class

---
+++ src/main/java/at/campus02/bsd/Cocktail.java
@@ -0,0 +1,49 @@
+/*
+ * Cocktail
+ * Represents Cocktail
+ * Author: Daniel Hubmann, Caroline Meixner
+ * Last Change: 27.06.2023
+ */
+
+package at.campus02.bsd;
+
+public class Cocktail extends Drink{
+    /**
+     * Creates a Drink object with given name
+     *
+     * @param name name of the drink
+     */
+    public Cocktail(String name) {
+        super(name);
+    }
+
+    /**
+     * Calculates and returns volume of drink
+     *
+     * @return the volume of drink in liter
+     */
+    @Override
+    public double getVolume() {
+        return 0;
+    }
+
+    /**
+     * Calculates and returns the alcohol percentage
+     *
+     * @return alcohol volume percent (e.g. 50)
+     */
+    @Override
+    public double getAlcoholPercent() {
+        return 0;
+    }
+
+    /**
+     * Gives information if drink is alcoholic or not
+     *
+     * @return true when alcoholic liquids are present, otherwise false
+     */
+    @Override
+    public boolean isAlcoholic() {
+        return false;
+    }
+}
