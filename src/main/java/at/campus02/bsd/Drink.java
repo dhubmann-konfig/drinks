0313729fcfbfa0ed852b4744127e578c7595451a
Caroline Meixner <01610205@edu.campus02.at>
2023-06-29 15:32:27 +0200

    Add javadoc to Drink class

--- src/main/java/at/campus02/bsd/Drink.java
+++ src/main/java/at/campus02/bsd/Drink.java
@@ -2,19 +2,25 @@
  * Drink
  * Represents Drink
  * Author: Daniel Hubmann, Caroline Meixner
- * Last Change: 22.06.2023
+ * Last Change: 29.06.2023
  */
 
 package at.campus02.bsd;
 
 /**
  * Abstract base class for concrete drink classes
+ *
+ * Contains methods to calculate volume, alc/vol and if a drink
+ * is alcoholic.
+ *
+ * @author Caroline Meixner
  */
 public abstract class Drink {
     /**
      * name of the drink
      */
     protected String name;
+
     /**
      * Creates a Drink object with given name
      *
@@ -24,18 +30,21 @@ public abstract class Drink {
     public Drink(String name) {
         this.name = name;
     }
+
     /**
      * Calculates and returns volume of drink
      *
      * @return the volume of drink in liter
      */
     public abstract double getVolume();
+
     /**
      * Calculates and returns the alcohol percentage
      *
      * @return alcohol volume percent (e.g. 50)
      */
     public abstract double getAlcoholPercent();
+
     /**
      * Gives information if drink is alcoholic or not
      *
