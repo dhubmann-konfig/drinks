1c285cc606b59f763fec0a300cd52dc92c0f0380
Daniel Hubmann <01130491@edu.campus02.at>
2023-06-30 09:20:44 +0200

    Implement SimpleDrinkTest class
    Implemented JUnit tests for the SimpleDrink class. Tests are covering
    100% as specified.

--- src/test/java/at/campus02/bsd/SimpleDrinkTest.java
+++ src/test/java/at/campus02/bsd/SimpleDrinkTest.java
@@ -7,8 +7,49 @@
 
 package at.campus02.bsd;
 
+import org.junit.jupiter.api.*;
+
 public class SimpleDrinkTest {
 
-	SimpleDrink sd;
+	SimpleDrink simpleDrink;
+	Liquid liquid;
+
+	@BeforeEach
+	public void setUp() {
+		liquid = new Liquid("Gin", 0.06, 37.5);
+		simpleDrink = new SimpleDrink("Martini", liquid);
+	}
+
+	@Test
+	public void getVolumeTest() {
+		double expectedVolume = 0.06;
+		liquid.setVolume(expectedVolume);
+		double result = simpleDrink.getVolume();
+		Assertions.assertEquals(expectedVolume, result, 0.0);
+	}
+
+	@Test
+	public void getAlcoholPercentTest() {
+		double expectedAlcoholPercent = 37.5;
+		liquid.setAlcoholPercent(expectedAlcoholPercent);
+		double result = simpleDrink.getAlcoholPercent();
+		Assertions.assertEquals(expectedAlcoholPercent, result, 0.0);
+	}
+
+	@Test
+	public void isAlcoholicTest_AlcoholPercentGreaterThanZero() {
+		double alcoholPercent = 37.5;
+		liquid.setAlcoholPercent(alcoholPercent);
+		boolean result = simpleDrink.isAlcoholic();
+		Assertions.assertTrue(result);
+	}
+
+	@Test
+	public void isAlcoholicTest_AlcoholPercentIsZero() {
+		double alcoholPercent = 0.0;
+		liquid.setAlcoholPercent(alcoholPercent);
+		boolean result = simpleDrink.isAlcoholic();
+		Assertions.assertFalse(result);
+	}
 
 }
