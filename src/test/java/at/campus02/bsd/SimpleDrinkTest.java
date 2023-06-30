55eb44747a8c672ed4f9d2e942b9d6c0c7dea2ad
Daniel Hubmann <01130491@edu.campus02.at>
2023-06-30 08:28:28 +0200

    Create test/java folder & SimpleDrinkTest class

---
+++ src/test/java/at/campus02/bsd/SimpleDrinkTest.java
@@ -0,0 +1,14 @@
+/*
+ * SimpleDrinkTest
+ * Represents JUnit Test for Simple Drink
+ * Author: Daniel Hubmann, Caroline Meixner
+ * Last Change: 30.06.2023
+ */
+
+package at.campus02.bsd;
+
+public class SimpleDrinkTest {
+
+	SimpleDrink sd;
+
+}
