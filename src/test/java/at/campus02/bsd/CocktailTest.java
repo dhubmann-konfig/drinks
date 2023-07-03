52f2a9937e9c497c9363ffe5d489783ef5673fff
Caroline Meixner <01610205@edu.campus02.at>
2023-07-03 10:05:48 +0200

    Add javadoc for CocktailTest class

--- src/test/java/at/campus02/bsd/CocktailTest.java
+++ src/test/java/at/campus02/bsd/CocktailTest.java
@@ -2,7 +2,7 @@
  * CocktailTest
  * Represents JUnit Test for Cocktail
  * Author: Daniel Hubmann, Caroline Meixner
- * Last Change: 01.07.2023
+ * Last Change: 03.07.2023
  */
 
 package at.campus02.bsd;
@@ -16,13 +16,33 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * JUnit Test class for Cocktail class.
+ *
+ * Contains test cases for getVolume(), getAlcoholPercent(),
+ * isAlcoholic() methods.
+ *
+ * @author Caroline Meixner
+ *
+ */
+
 public class CocktailTest {
 
+    /**
+     * Declaration of the Cocktail object
+     */
     Cocktail cocktail;
 
+    /**
+     * Tests for alcoholic drinks
+     */
     @Nested
     public class NestedBlock {
 
+        /**
+         * Creates liquid objects and liquidList to create
+         * new Cocktail object for testing
+         */
         @BeforeEach
         void setup() {
             Liquid alcoholicLiquid = new Liquid("Gin", 0.06, 37.5);
@@ -33,24 +53,36 @@ public class CocktailTest {
             cocktail = new Cocktail("Gin Tonic", liquidList);
         }
 
+        /**
+         * Tests return value of getVolume() method
+         */
         @Test
         void volumeTest() {
             double volume = cocktail.getVolume();
             Assertions.assertEquals(0.18, volume);
         }
 
+        /**
+         * Tests return value of getAlcoholPercent() method
+         */
         @Test
         void alcoholPercentTest() {
             double alcoholPercent = cocktail.getAlcoholPercent();
             Assertions.assertEquals(12.5, alcoholPercent);
         }
 
+        /**
+         * Tests return value of isAlcoholic() method with alcoholic drink
+         */
         @Test
         void isAlcoholicTest_True() {
             boolean isAlcoholic = cocktail.isAlcoholic();
             Assertions.assertTrue(isAlcoholic);
         }
-        
+
+        /**
+         * Tests return value of getName() method
+         */
         @Test
     	public void getNameTest() {
     		String expectedResult = "Gin Tonic";
@@ -60,6 +92,9 @@ public class CocktailTest {
     	}
     }
 
+    /**
+     * Tests return value of isAlcoholic() method with non-alcoholic drink
+     */
     @Test
     public void isAlcoholicTest_False() {
         Liquid pineappleJuice = new Liquid("Pineapple Juice", 0.15, 0);
