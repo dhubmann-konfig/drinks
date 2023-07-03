4b15dac2e0b8fe6226214312015791f6f8eb829f
Caroline Meixner <01610205@edu.campus02.at>
2023-07-03 10:06:22 +0200

    Add javadoc for StringQueueTest class

--- src/test/java/at/campus02/bsd/StringQueueTest.java
+++ src/test/java/at/campus02/bsd/StringQueueTest.java
@@ -2,7 +2,7 @@
  * StringQueueTest
  * Represents JUnit Test for String Queue
  * Author: Daniel Hubmann, Caroline Meixner
- * Last Change: 01.07.2023
+ * Last Change: 03.07.2023
  */
 
 package at.campus02.bsd;
@@ -11,25 +11,51 @@ import org.apache.logging.log4j.core.util.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.NoSuchElementException;
+
+/**
+ * JUnit Test class for StringQueue class.
+ *
+ * Contains test cases for offer(), poll(), remove(),
+ * peek(), element() methods.
+ *
+ * @author Caroline Meixner
+ *
+ */
 
 public class StringQueueTest {
 
+    /**
+     * Declaration of the StringQueue object
+     */
     StringQueue stringQueue;
+
+    /**
+     * Test string
+     */
     String string = "text";
 
+    /**
+     * Creates new StringQueue object with max size of 1
+     */
     @BeforeEach
     public void setUp() {
         stringQueue = new StringQueue(1);
     }
 
+
+    /**
+     * Tests if a new string can be added to an empty queue
+     */
     @Test
     public void offerTest_HasSpace() {
         boolean result = stringQueue.offer(string);
         Assertions.assertTrue(result);
     }
 
+    /**
+     * Tests if a new string can be added to a full queue
+     */
     @Test
     public void offerTest_Full() {
         stringQueue.offer(string);
@@ -37,6 +63,9 @@ public class StringQueueTest {
         Assertions.assertFalse(result);
     }
 
+    /**
+     * Tests return value of poll() method with a filled queue
+     */
     @Test
     public void pollTest_Filled() {
         stringQueue.offer(string);
@@ -44,6 +73,9 @@ public class StringQueueTest {
         Assertions.assertEquals(string, result);
     }
 
+    /**
+     * Tests return value of poll() method with an empty queue
+     */
     @Test
     public void pollTest_Empty() {
         Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
@@ -51,6 +83,9 @@ public class StringQueueTest {
         });
     }
 
+    /**
+     * Tests return value of remove() method with a filled queue
+     */
     @Test
     public void removeTest_Filled() {
         stringQueue.offer(string);
@@ -58,6 +93,9 @@ public class StringQueueTest {
         Assertions.assertEquals(string, result);
     }
 
+    /**
+     * Tests return value of remove() method with an empty queue
+     */
     @Test
     public void removeTest_Empty() {
         Assertions.assertThrows(NoSuchElementException.class, () -> {
@@ -67,6 +105,9 @@ public class StringQueueTest {
         });
     }
 
+    /**
+     * Tests return value of peek() method with a filled queue
+     */
     @Test
     public void peekTest_Filled() {
         stringQueue.offer(string);
@@ -74,12 +115,18 @@ public class StringQueueTest {
         Assertions.assertEquals(string, result);
     }
 
+    /**
+     * Tests return value of peek() method with an empty queue
+     */
     @Test
     public void peekTest_Empty() {
         String result = stringQueue.peek();
         Assertions.assertNull(result);
     }
 
+    /**
+     * Tests return value of element() method with a filled queue
+     */
     @Test
     public void elementTest_Filled() {
         stringQueue.offer(string);
@@ -87,6 +134,9 @@ public class StringQueueTest {
         Assertions.assertEquals(string, result);
     }
 
+    /**
+     * Tests return value of element() method with an empty queue
+     */
     @Test
     public void elementTest_Empty() {
         Assertions.assertThrows(NoSuchElementException.class, () -> {
